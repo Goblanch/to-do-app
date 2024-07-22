0686629999155e46c29c2fbd0a6a9a4af0935e20
Gonzalo <gonzaloblanchdev@gmail.com>
2024-07-22 19:33:01 +0200

    feat($root): add task module

---
+++ .venv/task.py
@@ -0,0 +1,29 @@
+import tkinter
+import tkinter.font as tkFont
+import ttkbootstrap as ttk
+
+
+class Task:
+    def __init__(self, master, text="", font="", status= False) -> None:
+        self.font : str = font
+        self.task_frame = ttk.Frame(master=master)
+        self.task_label = ttk.Label(master=self.task_frame, text=text, font=font)
+        self.task_check_box = ttk.Checkbutton(master=self.task_frame, command=self.check_task)
+        # unchecking button by default
+        self.task_check_box.state(['!alternate'])
+        self.task_status : bool = status
+
+    def pack(self, padx=0, pady=0) -> None:
+        self.task_check_box.pack(side="left")
+        self.task_label.pack(side="left")
+        self.task_frame.pack(side="left", padx=padx, pady=pady)
+
+    # TODO: function implementation
+    def check_task(self) -> None:
+        self.task_status = not self.task_status
+        if(self.task_status):
+            strikethrough_font = tkFont.Font(family="Calibri", size=18, overstrike=1)
+            self.task_label.configure(font=strikethrough_font)
+        else:
+            normal_font = tkFont.Font(family="Calibri", size=18, overstrike=0)
+            self.task_label.configure(font=normal_font)
